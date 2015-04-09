f5e70d6c515c544782c9d125f0225b360c72dcbf
bwjackson <bwjackson@oakland.edu>
2015-04-08 20:53:56 -0400

    Removed Settings Buttons

--- app/src/main/java/cse202/jarekwojo/pcpartplanner/AddPartActivity.java
+++ app/src/main/java/cse202/jarekwojo/pcpartplanner/AddPartActivity.java
@@ -148,9 +148,9 @@ public class AddPartActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        /*if (id == R.id.action_settings) {
             return true;
-        }
+        }*/
 
         return super.onOptionsItemSelected(item);
     }
--- app/src/main/java/cse202/jarekwojo/pcpartplanner/MainActivity.java
+++ app/src/main/java/cse202/jarekwojo/pcpartplanner/MainActivity.java
@@ -99,9 +99,9 @@ public class MainActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        /*if (id == R.id.action_settings) {
             return true;
-        }
+        }*/
 
         return super.onOptionsItemSelected(item);
     }
